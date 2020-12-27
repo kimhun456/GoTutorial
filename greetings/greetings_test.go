49f8cde9ebfb4e2b6c41ee9c3cd8d522f94a8391
Hyunjae Kim <kimhun456@gmail.com>
2020-12-27 19:04:42 +0900

    add unit test

---
+++ greetings/greetings_test.go
@@ -0,0 +1,26 @@
+package greetings
+
+import (
+    "testing"
+    "regexp"
+)
+
+func TestHelloName(t *testing.T) {
+    name := "Hyunjae"
+    want := regexp.MustCompile(`\b`+name+`\b`)
+    msg, err := Hello("Hyunjae")
+
+    if !want.MatchString(msg) || err != nil {
+        t.Fatalf(`Hello("Hyunjae") = %q, %v, want match for %#q, nil`, msg, err, want)
+    }
+}
+
+
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+    msg, err := Hello("")
+    if msg != "" || err == nil {
+        t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+    }
+}
